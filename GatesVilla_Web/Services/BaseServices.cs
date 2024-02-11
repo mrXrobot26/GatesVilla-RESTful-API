8e5304630445a27fa5769be18b7acb2ef6bc7997
Abdo Elbessa <zezoelbessa10@egyeagles.com>
2024-02-11 23:55:29 +0200

    Send Request

---
+++ GatesVilla_Web/Services/BaseServices.cs
@@ -0,0 +1,72 @@
+﻿using GatesVilla_Utility;
+using GatesVilla_Web.Models;
+using GatesVilla_Web.Services.IServices;
+using GatesVillaAPI.Models.Models.APIResponde;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace GatesVilla_Web.Services
+{
+    public class BaseServices : IBaseServices
+    {
+        public APIResponse apiResponse { get; set; }
+        IHttpClientFactory httpClient { get; set; }
+        public BaseServices(IHttpClientFactory httpClient)
+        {
+            this.httpClient = httpClient;
+            apiResponse = new();
+        }
+
+        public async Task<T> SendAsync<T>(APIRequest apiRequest)
+        {
+            try
+            {
+                var client = httpClient.CreateClient("VillaApi");
+                HttpRequestMessage msg = new HttpRequestMessage();
+                msg.Headers.Add("Content-Type", "application/json");
+                msg.RequestUri = new Uri(apiRequest.Url);
+                if (apiRequest.Data != null)
+                {
+                    msg.Content = new StringContent(JsonConvert.SerializeObject(apiRequest.Data), Encoding.UTF8, "application/json");
+                }
+                switch (apiRequest.ApiType)
+                {
+                    case SD.APIType.POST:
+                        msg.Method = HttpMethod.Post;
+                        break;
+                    case SD.APIType.PUT:
+                        msg.Method = HttpMethod.Put;
+                        break;
+                    case SD.APIType.DELETE:
+                        msg.Method = HttpMethod.Delete;
+                        break;
+                    default:
+                        msg.Method = HttpMethod.Get;
+                        break;
+                }
+
+                HttpResponseMessage responseMessage = null;
+
+                responseMessage = await client.SendAsync(msg);
+
+                var apiContant = await responseMessage.Content.ReadAsStringAsync();
+                var APIResponse = JsonConvert.DeserializeObject<T>(apiContant);
+                return APIResponse;
+
+
+            }
+            catch (Exception ex)
+            {
+                var dto = new APIResponse
+                {
+                    ErrorMessages = new List<string> { ex.Message },
+                    IsSuccess = false
+                };
+                var res = JsonConvert.SerializeObject(dto);
+                var APIResponse = JsonConvert.DeserializeObject<T>(res);
+                return APIResponse;
+            }
+        
+        }
+    }
+}
---
+++ GatesVilla_Web/Services/IServices/IBaseServices.cs
@@ -0,0 +1,13 @@
+﻿using GatesVilla_Web.Models;
+using GatesVillaAPI.Models.Models.APIResponde;
+using static GatesVilla_Utility.SD;
+
+namespace GatesVilla_Web.Services.IServices
+{
+    public interface IBaseServices
+    {
+        public APIResponse apiResponse { get; set; }
+        public Task<T> SendAsync<T>(APIRequest apiRequest);
+
+    }
+}
